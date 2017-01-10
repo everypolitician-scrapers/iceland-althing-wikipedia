40e59d485a5a94ff532804dbf547ffdce1833c2c
Tony Bowden <tony@tmtm.com>
2017-01-10 18:26:01 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'pry'
@@ -10,33 +11,33 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_term(id, url)
   noko = noko_for(url)
   noko.xpath('//table[.//th[.="Þingmaður"]]//tr[td]').each do |tr|
     tds = tr.css('td')
-    area = id.to_i > 1990 ?  tr.xpath('preceding::h2/span[@class="mw-headline"]').last.text : tr.xpath('preceding::h4/span[@class="mw-headline"]').last.text 
-    data = { 
-      name: tds[2].css('a').text,
+    area = id.to_i > 1990 ? tr.xpath('preceding::h2/span[@class="mw-headline"]').last.text : tr.xpath('preceding::h4/span[@class="mw-headline"]').last.text
+    data = {
+      name:     tds[2].css('a').text,
       wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
-      party: tds[3].text.tidy,
-      area: area.tidy,
-      term: id,
-      source: url,
+      party:    tds[3].text.tidy,
+      area:     area.tidy,
+      term:     id,
+      source:   url,
     }
     # puts data
-    ScraperWiki.save_sqlite([:name, :wikiname, :term], data)
+    ScraperWiki.save_sqlite(%i(name wikiname term), data)
   end
 end
 
-terms = { 
+terms = {
   '2013' => 'https://is.wikipedia.org/wiki/Kj%C3%B6rnir_al%C3%BEingismenn_2013',
   '2009' => 'https://is.wikipedia.org/wiki/Kj%C3%B6rnir_al%C3%BEingismenn_2009',
   '2007' => 'https://is.wikipedia.org/wiki/Kj%C3%B6rnir_al%C3%BEingismenn_2007',
